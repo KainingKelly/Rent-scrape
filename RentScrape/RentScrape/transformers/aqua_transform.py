0612ffac66381dff72356c5919bd7ee0bb0a5430
KainingKelly <kainingjiangkelly@gmail.com>
2026-01-06 17:46:18 -0600

    Aqua Scrape

--- RentScrape/RentScrape/transformers/aqua_transform.py
+++ RentScrape/RentScrape/transformers/aqua_transform.py
@@ -12,11 +12,17 @@ class AquaTransform:
             self.my_dict = json.loads(f.read())
 
     def transform(self) -> list[pd.Dataframe]:
-        layouts = self.my_dict["units_data"]["layouts"]
+        layouts = self.my_dict["units_data"]["layouts"] # list[dict]
         floorplan_df = pd.DataFrame(layouts)
         floorplan_df = floorplan_df[["area", "bathrooms", "bedrooms", "name"]]
+        floorplan_df = floorplan_df.rename(columns={"area": "size", "bathrooms":"baths", "bedrooms":"beds"})
+        floorplan_df["img_path"] = "N/A"
+        floorplan_df["building"] = "Aqua"
         units = self.my_dict["units_data"]["units"]
         units_df = pd.DataFrame(units)
-        units_df = units_df[["area","availableOn","layoutName","name","price"]]
+        units_df = units_df[["availableOn","layoutName","name","price"]]
+        units_df = units_df.rename(columns={"availableOn":"date_available", "layoutName":"floorplan", "name":"unit_number"})
+        units_df["as_of"] = datetime.date.today()
+        units_df["building"] = "Aqua"
 
         return units_df, floorplan_df
